a5f099ea0f75fddcdbd76845c854cabd802248c5
Rene Kabanda <renerugwiro@yahoo.fr>
2023-02-01 14:32:46 +0000

    solution for e-11

--- src/11-record.problem.ts
+++ src/11-record.problem.ts
@@ -1,7 +1,11 @@
 import { expect, it } from "vitest";
 
+type CacheType = {
+  [id: string]: string;
+}
+
 const createCache = () => {
-  const cache = {};
+  const cache: CacheType = {};
 
   const add = (id: string, value: string) => {
     cache[id] = value;
